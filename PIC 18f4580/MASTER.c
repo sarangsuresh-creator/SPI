42eb766df295c9295b6d922dc3aaf83470594fdc
SARANG SURESH <sureshsarang44@gmail.com>
2026-05-14 12:34:47 +0530

    Update MASTER.c

--- PIC 18f4580/MASTER.c
+++ PIC 18f4580/MASTER.c
@@ -6,58 +6,61 @@ void mychar(char d);
 void uart_initiate(void);
 
 void SPI_Master_Init(void);
-void SPI_Master_Write(unsigned char data);
 unsigned char receivedData;
 int i;
 void main(void)
 {
-    SPI_Master_Init();
+    ADCON1 = 0x0F;
     uart_initiate();
+    SPI_Master_Init();
+    
 
     while(1)
     {
+         SSPIF = 0;
+         RA5 = 0;
 
             SSPBUF = '0'+i++;
-            SSPIF=0;
-            while(!PIR1bits.SSPIF);
+            while(!SSPIF);
             receivedData = SSPBUF;
+           
+            RA5 = 1;
             string("received character:");
             mychar(receivedData);
-            mychar('\n\r');
+           string("\r\n");
             __delay_ms(1000); 
     }
 }
 
 void SPI_Master_Init(void)
 {
-    TRISC = 0x90;
-    SSPSTAT = 0x00; 
+    TRISC|=0x10;
+    TRISA = 0X00;
+    RA5 = 1;
+    SSPSTAT = 0x40; 
     SSPCON1 = 0x22;
     PIR1bits.SSPIF = 0;
 }
 
-void SPI_Master_Write(unsigned char data)
-{
-    SSPBUF = data;
-    while(!PIR1bits.SSPIF);
-    SSPIF = 0;
-}
 void uart_initiate(){
+  TRISC = 0x80;
   TXSTA = 0X24;
   RCSTA = 0X90;
   SPBRG = (((_XTAL_FREQ/baud_rate)/16)-1);
 }
 void string(char *s){
     while(*s){
-        TXREG = *s++;
-        while(!TXIF);
-        TXIF = 0;
+       while(!TXIF);
+       TXREG = *s++;
+      
+       
     }
 }
 void mychar(char d){
-    TXREG = d;
-    while(!TXIF);
-    TXIF = 0;
+  while(!TXIF);
+  TXREG = d;
+   
+   
 }
 
 
